3a74692440e9976340f9aa751916a3acd8bbb9b9
Luana Braga <101228187+LuanaBraga01@users.noreply.github.com>
2022-06-08 23:45:38 -0300

    Projetofinalizado.js
    Projeto final da unidade de lógica de programação

---
+++ pcescolhe.js
@@ -0,0 +1,83 @@
+const getUserChoice = (userInput) =>
+{
+  if(userInput.toLowerCase () == "pedra"){
+    return"pedra";
+
+  } else if(userInput.toLowerCase () == "papel"){
+    return"papel";
+
+  }else if(userInput.toLowerCase () == "tesoura"){
+    return"tesoura";
+
+  }else{
+    return("Opção Inválida")
+    }
+}
+
+function getComputerChoice ()
+{
+  let numero = Math.floor(Math.random() * 3);
+    
+  switch (numero){
+    case 0:
+      return "pedra"
+      break;
+    case 1:
+      return "papel"
+      break;
+    case 2:
+      return "tesoura"
+      break;
+  }
+}
+
+const determineWinner = (userChoice, computerChoice) => 
+{
+  if (userChoice == computerChoice){
+    return "Jogo empatado"
+  }
+    else if(userChoice == "pedra"){
+      if(computerChoice == "papel"){
+        return "O computador ganhou!"
+      }else{
+        return "Você ganhou!"
+      }
+  }
+
+  if(userChoice == "papel"){
+    if(computerChoice == "tesoura"){
+      return "O computador ganhou!"
+    }else{
+      return "Você ganhou"
+    }
+  }
+
+  if(userChoice == "tesoura"){
+    if(computerChoice == "pedra"){
+      return "O computador ganhou!"
+    }else{
+      return "Você ganhou!"
+    }
+  }
+
+}
+
+const playGame = () => {
+  
+  const userChoice = getUserChoice(prompt("Digite sua opção"));
+  const computerChoice = getComputerChoice();
+  
+  if(userChoice == "papel" || userChoice == "pedra" || userChoice == "tesoura"){
+ 
+    console.log("Usuário: " + userChoice);
+    console.log("Computador: " + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice))
+      
+  }else{
+
+    console.log("Opção inválida")
+    
+  }
+}
+
+playGame()
